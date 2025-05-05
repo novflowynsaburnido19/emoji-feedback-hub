b22069a6492a806b6000d8d2c182e93c054b7ca1
wyn <126950082+novflowynsaburnido19@users.noreply.github.com>
2025-05-05 11:14:47 +0800

    Delete api directory

--- api/feedbackModel.js
+++
@@ -1,9 +0,0 @@
-
-import mongoose from 'mongoose';
-
-const FeedbackSchema = new mongoose.Schema({
-  emoji: String,
-  comment: String,
-}, { timestamps: true });
-
-export default mongoose.models.Feedback || mongoose.model('Feedback', FeedbackSchema);
